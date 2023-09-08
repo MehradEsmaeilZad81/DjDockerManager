fee6a0d21b1910f8eb0803d9664a9ca7bc9163af
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-09-08 13:33:29 +0330

    Create "App" model and register in admin panel
    - Added a new model called "App" to represent application information.
      - Fields: name, image, envs (JSONField), and command.
    - Registered the "App" model in the admin panel for easy management.
      - Customized the admin panel display with list_display, list_filter,
        search_fields, and ordering options.
    
    This commit sets up the foundation for managing application data in the Django project.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import App
 
 # Register your models here.
+
+
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'command')
+    list_filter = ('name', 'image')
+    search_fields = ('name', 'image')
+    ordering = ('name',)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class App(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    envs = models.JSONField()
+    command = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name

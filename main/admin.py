7bc3f79c2b00b4c5b034cebe1e5dcf0e52efa558
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-09-10 11:51:45 +0330

    Add Run model and register it in admin panel
    - Created the Run model to represent execution runs of App instances.
    - Added fields for app, start time, parameters, and status.
    - Registered the Run model in the admin panel for easier management and viewing of run data.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import App
+from .models import App, Run
 
 # Register your models here.
 
@@ -10,3 +10,11 @@ class AppAdmin(admin.ModelAdmin):
     list_filter = ('name', 'image')
     search_fields = ('name', 'image')
     ordering = ('name',)
+
+
+@admin.register(Run)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('app', 'start_time', 'status')
+    list_filter = ('app', 'start_time', 'status')
+    search_fields = ('app', 'start_time', 'status')
+    ordering = ('app', 'start_time', 'status')
--- main/models.py
+++ main/models.py
@@ -11,3 +11,21 @@ class App(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Run(models.Model):
+    RUNNING = 'Running'
+    FINISHED = 'Finished'
+
+    STATUS_CHOICES = [
+        (RUNNING, 'Running'),
+        (FINISHED, 'Finished'),
+    ]
+
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    parameters = models.JSONField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"Run of {self.app.name} ({self.start_time})"
